4e844de9fcb51904efc8867556fdfe1070476ce5
KanyeWestStudio <gabizinnhhhh@gmail.com>
2026-09-07 08:42:58 -0700

    Add overlayMain function for floatwing integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:safestrap/app.dart';
+import 'package:flutter_floatwing/flutter_floatwing.dart';
+
+import 'app.dart';
+import 'overlay_menu.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const SafestrapApp());
+}
+
+@pragma('vm:entry-point')
+void overlayMain() {
+  runApp(const OverlayMenu().floatwing(app: true));
 }
