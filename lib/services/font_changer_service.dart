de9578beab7cb7dc9eb7498ed657cb2cf150f2a0
KanyeWestStudio <gabizinnhhhh@gmail.com>
2026-09-08 11:55:36 -0600

    Add permission_handler import to font_changer_service

--- lib/services/font_changer_service.dart
+++ lib/services/font_changer_service.dart
@@ -1,12 +1,12 @@
 // lib/services/font_changer_service.dart
 import 'dart:io';
-import 'package:permission_handler/permission_handler.dart';
 import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../models/font_preset.dart';
 
 class FontChangerService {
